fe3278b440a4bd51ec1c90b5d50f44593acd1f39
Avaq <aldwin.vlasblom@gmail.com>
2012-05-08 13:07:42 +0200

    Added the jQuery plugin.

---
+++ plugins/jquery/plugin.php
@@ -0,0 +1,6 @@
+<?php namespace plugins; if(!defined('TX')) die('No direct access.'); ?>
+<!--  <script type="text/javascript" src="--><?php //echo $plugin ?><!--jquery.min.js"></script>-->
+  <script type="text/javascript" src="http://code.jquery.com/jquery.js"></script>
+  <script type="text/javascript" src="<?php echo $plugin ?>jquery.hashchange.js"></script>
+  <script type="text/javascript" src="<?php echo $plugin ?>jquery.matches.js"></script>
+  <script type="text/javascript" src="<?php echo $plugin ?>jquery.form.js"></script>
