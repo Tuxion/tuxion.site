3fde46ee6dddb766eaf39ca23c495678c4af12af
Bart Roorda <bart@tuxion.nl>
2012-10-09 00:23:28 +0200

    Added plugin: momentjs.

--- plugins/momentjs/plugin.php
+++ plugins/momentjs/plugin.php
@@ -1,2 +1,4 @@
 <?php namespace plugins; if(!defined('TX')) die('No direct access.'); ?>
   <script type="text/javascript" src="<?php echo $plugin; ?>moment.min.js"></script>
+  <script type="text/javascript" src="<?php echo $plugin; ?>lang/nl.js"></script>
+  
